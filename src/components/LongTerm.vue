9999814d68797f0b2bea45dc795efd57747054be
Lukasz Ferenc <lukasz.ferenc@itvision.pl>
2021-04-19 20:08:09 +0200

    format

--- src/components/LongTerm.vue
+++ src/components/LongTerm.vue
@@ -1,149 +1,137 @@
 <template>
-    <div id="test">
-        <form>
-            Wybierz miasto<br>
-            
-            <select v-model ="City" @change="getData()" id="standard-select">
-            
-                <option v-for="city in Cities" v-bind:key="city">{{city.City}}</option>
-            </select>
-            
-        </form>
-        <br>
+  <div id="test">
+    <form>
+      Wybierz miasto<br />
 
-        <h1>{{City}}</h1>
-        <div class="center flexBlock">
-          
-            <table v-show=showChart>
+      <select v-model="City" @change="getData()" id="standard-select">
+        <option v-for="city in Cities" v-bind:key="city">{{
+          city.City
+        }}</option>
+      </select>
+    </form>
+    <br />
+
+    <h1>{{ City }}</h1>
+    <div class="center flexBlock">
+      <table v-show="showChart">
         <thead>
-        <tr>
-            
-        </tr>
-        <th>Data</th>
-        <th> Temperatura </th>
-        <th> Opady </th>
-        <th> Wiatr </th>
+          <tr></tr>
+          <th>Data</th>
+          <th>Temperatura</th>
+          <th>Opady</th>
+          <th>Wiatr</th>
 
-        <th></th>
-        <th></th>
+          <th></th>
+          <th></th>
         </thead>
         <tbody id="show">
-        <tr v-for="(element,index) in info" v-bind:key="index">
-          
-        <td>{{element.Date}} </td>
-        <td>{{element.Temperature}} °C </td>
-        <td>{{element.Rain}} mm / 12h</td>
-        <td>{{element.Wind}} km/h</td>
-      
-       
-        </tr>
+          <tr v-for="(element, index) in info" v-bind:key="index">
+            <td>{{ element.Date }}</td>
+            <td>{{ element.Temperature }} °C</td>
+            <td>{{ element.Rain }} mm / 12h</td>
+            <td>{{ element.Wind }} km/h</td>
+          </tr>
         </tbody>
-        </table>
-        </div>
-        <line-chart v-show="showChart" :data="chartData"></line-chart>    
+      </table>
     </div>
-
+    <line-chart v-show="showChart" :data="chartData"></line-chart>
+  </div>
 </template>
 <script>
-var showChart=new Boolean;
+var showChart = new Boolean();
 
- import axios from 'axios';
- import{ApiKey1} from '../main';
+import axios from "axios";
+import { ApiKey1 } from "../main";
 //import CitiesJson from '../components/Cities.json';
 //import Test from '../components/long.json';
-export default{
-    
-      data() {
+export default {
+  data() {
     return {
-      
       error: null,
-      info:[],
-      Cities:[],
-      City:'',
+      info: [],
+      Cities: [],
+      City: "",
       dataentry: null,
       datalabel: null,
-      chartData:{},
-      charLabel:[],
+      chartData: {},
+      charLabel: [],
       showChart,
-      test:[]
-          
-    
-      
-    }
+      test: [],
+    };
   },
-  methods:{
+  methods: {
+    getData() {
+      //axios.get('https://607740cc1ed0ae0017d6ab45.mockapi.io/Test')
 
-      getData()
-      {
-           
-       //axios.get('https://607740cc1ed0ae0017d6ab45.mockapi.io/Test')
+      axios
+        .get(
+          "https://my.api.mockaroo.com/LongTerm/" +
+            this.City +
+            ".json?key=" +
+            ApiKey1
+        )
+        .then((response) => {
+          this.info = response.data;
+          var obj1 = '{ "';
+          response.data.forEach((element) => {
+            obj1 += element.Date + '" : ' + element.Temperature + ', "';
+            this.charLabel = this.charLabel.concat(element.Date);
+          });
+          obj1 = obj1.slice(0, -3);
+          obj1 += "}";
+          console.log(obj1);
+          this.chartData = JSON.parse(obj1);
+        });
 
-        axios.get('https://my.api.mockaroo.com/LongTerm/'+this.City+'.json?key='+ApiKey1)
-        .then(response=>{this.info=response.data
-        var obj1='{ "'
-response.data.forEach(element => {
-  obj1+=element.Date+'" : '+element.Temperature+', "';
-  this.charLabel=this.charLabel.concat(element.Date)
-});
-obj1=obj1.slice(0,-3);
-obj1+="}";
-console.log(obj1);
-this.chartData=JSON.parse(obj1)})
+      //         fetch('https://607740cc1ed0ae0017d6ab45.mockapi.io/Test')
+      // .then(response => response.json())
+      // .then(json_data => this.info = json_data)
+      // .then(console.log(this.info))
 
-//         fetch('https://607740cc1ed0ae0017d6ab45.mockapi.io/Test')
-// .then(response => response.json())
-// .then(json_data => this.info = json_data)
-// .then(console.log(this.info))
-  
-  //.then(() => console.log(obj))
-        
-        
-      
-//console.log(this.info)
-//obj={ '01.04.2021' : 13, '02.04.2021' : 18, '03.04.2021' : 9, '04.04.2021' : 6, '05.04.2021' : 18, '06.04.2021' : 1, '07.04.2021' : 3, '08.04.2021' : 10, '09.04.2021' : 6, '10.04.2021' : 5, '11.04.2021' : 0, '12.04.2021' : 8, '13.04.2021' : 1, '14.04.2021' : 17, '15.04.2021' : 1, '16.04.2021' : 17, '17.04.2021' : 6, '18.04.2021' : 11, '19.04.2021' : 12, '20.04.2021' : 13, '21.04.2021' : 15, '22.04.2021' : 6, '23.04.2021' : 2, '24.04.2021' : 16, '25.04.2021' : 17, '26.04.2021' : 1, '27.04.2021' : 7, '28.04.2021' : 13, '29.04.2021' : 17, '30.04.2021' : 10 }
+      //.then(() => console.log(obj))
 
- this.showChart=true;
-      },
-      getCities()
-      {
-          axios.get('https://my.api.mockaroo.com/cities.json?key='+ApiKey1).then(response=>(this.Cities=response.data))
-          //this.Cities=CitiesJson
-      }
+      //console.log(this.info)
+      //obj={ '01.04.2021' : 13, '02.04.2021' : 18, '03.04.2021' : 9, '04.04.2021' : 6, '05.04.2021' : 18, '06.04.2021' : 1, '07.04.2021' : 3, '08.04.2021' : 10, '09.04.2021' : 6, '10.04.2021' : 5, '11.04.2021' : 0, '12.04.2021' : 8, '13.04.2021' : 1, '14.04.2021' : 17, '15.04.2021' : 1, '16.04.2021' : 17, '17.04.2021' : 6, '18.04.2021' : 11, '19.04.2021' : 12, '20.04.2021' : 13, '21.04.2021' : 15, '22.04.2021' : 6, '23.04.2021' : 2, '24.04.2021' : 16, '25.04.2021' : 17, '26.04.2021' : 1, '27.04.2021' : 7, '28.04.2021' : 13, '29.04.2021' : 17, '30.04.2021' : 10 }
 
+      this.showChart = true;
+    },
+    getCities() {
+      axios
+        .get("https://my.api.mockaroo.com/cities.json?key=" + ApiKey1)
+        .then((response) => (this.Cities = response.data));
+      //this.Cities=CitiesJson
+    },
   },
-beforeMount()
-{
-this.getCities();
-this.showChart=false;
-
-}
-
-}
+  beforeMount() {
+    this.getCities();
+    this.showChart = false;
+  },
+};
 </script>
 <style scoped>
-.center{
-	margin: 0 auto;
-	max-width: 1200px;
-	padding: 0 30px;
-    margin-bottom: 50px;
+.center {
+  margin: 0 auto;
+  max-width: 1200px;
+  padding: 0 30px;
+  margin-bottom: 50px;
 }
-.flexBlock{
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
+.flexBlock {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
-table{
-    width: 1000px;
-    margin: 0 auto;
+table {
+  width: 1000px;
+  margin: 0 auto;
 }
-th, td{
-    padding: 20px;
+th,
+td {
+  padding: 20px;
 }
 table {
   border-collapse: collapse;
 }
 tr {
-  border-bottom: 1pt solid #15202B;
+  border-bottom: 1pt solid #15202b;
 }
-
 </style>
--- src/components/Smog.vue
+++ src/components/Smog.vue
@@ -1,94 +1,84 @@
 <template>
-    <div id="test">
-        <h1>Zanieczyszczenie powietrza</h1>
-        <br>
-        <br>
-            <table v-show="false" class="table table-striped text-white">
-        <thead>
+  <div id="test">
+    <h1>Zanieczyszczenie powietrza</h1>
+    <br />
+    <br />
+    <table v-show="false" class="table table-striped text-white">
+      <thead>
         <th>Miasto</th>
-        <th> PM10 </th>
-        <th> PM2.5 </th>
-        <th> PM1 </th>
+        <th>PM10</th>
+        <th>PM2.5</th>
+        <th>PM1</th>
 
         <th></th>
         <th></th>
-        </thead>
-        <tbody id="show">
+      </thead>
+      <tbody id="show">
         <tr v-for="element in info" v-bind:key="element">
-        <td>{{element.City}}</td>
-        <td>{{element.PM10}} µg/m³</td>
-        <td>{{element.PM25}} µg/m³</td>
-        <td>{{element.PM1}} µg/m³</td>
-
-       
+          <td>{{ element.City }}</td>
+          <td>{{ element.PM10 }} µg/m³</td>
+          <td>{{ element.PM25 }} µg/m³</td>
+          <td>{{ element.PM1 }} µg/m³</td>
         </tr>
-        </tbody>
-        </table> 
-        <h1>PM10 µg/m³</h1>
-        <column-chart :data="chartData"></column-chart>
-        <br>
-        <h1>PM2.5 µg/m³</h1>
-        <column-chart :data="chartData1"></column-chart>
-        <br>
-        <h1>PM1 µg/m³</h1>
-        <column-chart :data="chartData2"></column-chart>
-        
-    </div>
+      </tbody>
+    </table>
+    <h1>PM10 µg/m³</h1>
+    <column-chart :data="chartData"></column-chart>
+    <br />
+    <h1>PM2.5 µg/m³</h1>
+    <column-chart :data="chartData1"></column-chart>
+    <br />
+    <h1>PM1 µg/m³</h1>
+    <column-chart :data="chartData2"></column-chart>
+  </div>
 </template>
-   <script src='//cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js'></script>
-    <script src='//unpkg.com/vue-chartjs@2.6.0/dist/vue-chartjs.full.min.js'></script>
-    <script src='//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.3.0/Chart.js'></script>
-    <script src='//unpkg.com/hchs-vue-charts@1.2.8'></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js"></script>
+<script src="//unpkg.com/vue-chartjs@2.6.0/dist/vue-chartjs.full.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.3.0/Chart.js"></script>
+<script src="//unpkg.com/hchs-vue-charts@1.2.8"></script>
 <script>
-
-import axios from 'axios';
-import{ApiKey1} from '../main';
-export default{
-      data() {
+import axios from "axios";
+import { ApiKey1 } from "../main";
+export default {
+  data() {
     return {
-      
       error: null,
-      info:[],
-chartData:{},
-chartData1:{},
-chartData2:{},
-    }
+      info: [],
+      chartData: {},
+      chartData1: {},
+      chartData2: {},
+    };
   },
-  methods:{
-      getData()
-      {
-          
-axios.get('https://my.api.mockaroo.com/smog1.json?key='+ApiKey1)
-//axios.get('https://607c6ec567e653001757407d.mockapi.io/Cities')
-.then(response=>{this.info=response.data
-        var obj1='{ "'
-        var obj2='{ "'
-        var obj3='{ "'
-response.data.forEach(element => {
-  obj1+=element.City+'" : '+element.PM10+', "';
-  obj2+=element.City+'" : '+element.PM25+', "';
-  obj3+=element.City+'" : '+element.PM1+', "';
- 
-});
-obj1=obj1.slice(0,-3);
-obj1+="}";
-obj2=obj2.slice(0,-3);
-obj2+="}";
-obj3=obj3.slice(0,-3);
-obj3+="}";
+  methods: {
+    getData() {
+      axios
+        .get("https://my.api.mockaroo.com/smog1.json?key=" + ApiKey1)
+        //axios.get('https://607c6ec567e653001757407d.mockapi.io/Cities')
+        .then((response) => {
+          this.info = response.data;
+          var obj1 = '{ "';
+          var obj2 = '{ "';
+          var obj3 = '{ "';
+          response.data.forEach((element) => {
+            obj1 += element.City + '" : ' + element.PM10 + ', "';
+            obj2 += element.City + '" : ' + element.PM25 + ', "';
+            obj3 += element.City + '" : ' + element.PM1 + ', "';
+          });
+          obj1 = obj1.slice(0, -3);
+          obj1 += "}";
+          obj2 = obj2.slice(0, -3);
+          obj2 += "}";
+          obj3 = obj3.slice(0, -3);
+          obj3 += "}";
 
-this.chartData=JSON.parse(obj1)
-this.chartData1=JSON.parse(obj2)
-this.chartData2=JSON.parse(obj3)
-})
-
-      }
-
+          this.chartData = JSON.parse(obj1);
+          this.chartData1 = JSON.parse(obj2);
+          this.chartData2 = JSON.parse(obj3);
+        });
+    },
   },
-beforeMount()
-{
-this.getData();
-
-}
-}
+  beforeMount() {
+    this.getData();
+  },
+};
 </script>
